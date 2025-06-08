e96999d33a5e24b1d07532a73c51662e838ef283
Rajkishor Thakur <rajkishort596@gmail.com>
2025-06-08 18:02:11 +0530

    Configured Tailwind CSS Based on Figma design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-extrabold text-center text-[#6B0606]">
+      <h1 className="text-4xl font-imperial text-[100px] text-center text-primary">
         Beauty Salon
       </h1>
+      <button className="btn-primary">Learn More</button>
     </>
   );
 }
