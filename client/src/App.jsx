57b285cb70e4d6f710089871b3aff6b0379004ad
Rajkishor Thakur <rajkishort596@gmail.com>
2025-06-08 09:21:08 +0530

    Initialised Frontend of Beauty Salon with tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-extrabold text-center text-[#6B0606]">
+        Beauty Salon
+      </h1>
+    </>
+  );
+}
+
+export default App;
